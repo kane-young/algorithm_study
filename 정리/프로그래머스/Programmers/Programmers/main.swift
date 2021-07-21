447d5141a5c7b55ed370092dbac8cc624d6cdf71
kane-young <lyw2100@naver.com>
2021-07-22 01:47:45 +0900

    Feat : 프로그래머스 크레인인형뽑기게임 풀이

--- 정리/프로그래머스/Programmers/Programmers/main.swift
+++ 정리/프로그래머스/Programmers/Programmers/main.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Programmers {
     func start() {
-        let result = 키패드누르기([1,3,4,5,8,2,1,4,5,9,5] , "right")
+        let result = 크레인인형뽑기게임([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4])
         print(result)
         //LRLLLRLLRRL
     }
---
+++ 정리/프로그래머스/Programmers/Programmers/크레인인형뽑기게임.swift
@@ -0,0 +1,50 @@
+//
+//  크레인인형뽑기게임.swift
+//  Programmers
+//
+//  Created by 이영우 on 2021/07/22.
+//
+
+import Foundation
+
+extension Programmers {
+    func 크레인인형뽑기게임(_ board:[[Int]], _ moves:[Int]) -> Int {
+        var board: [[Int]] = board
+        var basket: Stack = Stack()
+        
+        func rotateArray(_ num: Int) {
+            for i in 0..<board.count {
+                if board[i][num-1] != 0 {
+                    basket.push(board[i][num-1])
+                    board[i][num-1] = 0
+                    return
+                }
+            }
+        }
+        
+        for move in moves {
+            rotateArray(move)
+        }
+        return basket.result
+    }
+}
+
+struct Stack {
+    var array: [Int] = []
+    var result = 0
+    
+    mutating func push(_ num: Int) {
+        if let last = array.last {
+            if num == last {
+                array.removeLast()
+                result += 2
+                return
+            }
+        }
+        self.array.append(num)
+    }
+    
+    mutating func pop() {
+        array.removeLast()
+    }
+}
